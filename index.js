9ba9c4bf158a9e3ae8b9aae89375bba499ea281c
elycruz <603428+elycruz@users.noreply.github.com>
2021-11-27 03:40:25 -0500

    Progress on converting 'TestSuites' class to use 'TestSuite' class, to represent a 'test-suite'.

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const {log, warn, assert, error} = console,
     BEFORE_EACH_METHOD = Symbol('beforeEach'),
     TEST_SUITE_DEFINE = Symbol('testSuiteDefine'),
     TEST_SUITE_RUN = Symbol('testSuiteRun'),
+    TEST_CASE_DEFINE = Symbol('testCaseDefine'),
     TEST_CASE_RUN = Symbol('testCaseRun'),
     DATA = Symbol('data')
 ;
@@ -27,23 +28,11 @@ const {log, warn, assert, error} = console,
 export {jsonClone, assert, log, warn, error};
 
 class TestSuite {
-
-}
-
-export class TestSuites {
-    beforeAll = noop;
-    beforeEach = noop;
-    afterAll = noop;
-    afterEach = noop;
-    describe = noop;
-    it = noop;
+    name;
+    idx;
 
     [DATA] = {
-        suites: {
-            failed: 0,
-            passed: 0,
-        },
-        suitesMap: new Map(),
+        testsMap: new Map(),
         tests: {
             failed: 0,
             passed: 0,
@@ -51,30 +40,19 @@ export class TestSuites {
         timeElapsed: 0,
     };
 
-    constructor(options = {}) {
+    constructor(props = {}) {
+        Object.assign(this, {name: '', value: noop, idx: 0}, props);
         Object.defineProperties(this, {
-            it: {value: this[TEST_CASE_RUN].bind(this), configurable: false},
-            test: {value: this[TEST_CASE_RUN].bind(this), configurable: false},
-            describe: {value: this[TEST_SUITE_DEFINE].bind(this), configurable: false},
-            numSuitesPassed: {
-                get() {
-                    return this[DATA].suites.passed;
-                },
-                enumerable: true
-            },
-            numSuitesFailed: {
-                get() {
-                    return this[DATA].suites.count - this.numSuitesPassed;
-                },
-                enumerable: true
-            },
+            name: {value: this.name, configurable: false},
+            idx: {value: this.idx, configurable: false},
+            value: {value: this.value, configurable: false},
         });
-        Object.assign(this, options);
     }
 
-    [TEST_SUITE_DEFINE](name, fn) {
+    [TEST_CASE_DEFINE](name, fn) {
         _ensureArgsFormat(name, fn);
-        this[DATA].suitesMap.set(name, fn);
+        this[DATA].testsMap.set(name, fn);
+        return this;
     }
 
     [TEST_CASE_RUN](name, fn) {
@@ -105,7 +83,75 @@ export class TestSuites {
         }
     }
 
-    [TEST_SUITE_RUN](name, fn) {
+    [TEST_SUITE_RUN]() {
+        const {[DATA]: {testsMap}} = this,
+            entries = testsMap.entries(),
+            out = new Array(testsMap.size).fill(null, testsMap.size);
+        let i = 0;
+        for (const [k, f] of entries) {
+            out[i++] = this[TEST_CASE_RUN](k, f);
+        }
+        return Promise.all(out);
+    }
+}
+
+export class TestSuites extends TestSuite {
+    beforeAll = noop;
+    beforeEach = noop;
+    afterAll = noop;
+    afterEach = noop;
+    describe = noop;
+    it = noop;
+    test = noop;
+
+    [DATA] = {
+        suites: {
+            failed: 0,
+            passed: 0,
+        },
+        suitesMap: new Map(),
+        tests: {
+            failed: 0,
+            passed: 0,
+        },
+        timeElapsed: 0,
+    };
+
+    constructor(options = {}) {
+        super(options);
+        Object.defineProperties(this, {
+            it: {value: this[TEST_CASE_RUN].bind(this), configurable: false},
+            test: {value: this[TEST_CASE_RUN].bind(this), configurable: false},
+            describe: {value: this[TEST_SUITE_DEFINE].bind(this), configurable: false},
+            numSuitesPassed: {
+                get() {
+                    return this[DATA].suites.passed;
+                },
+                enumerable: true
+            },
+            numSuitesFailed: {
+                get() {
+                    return this[DATA].suites.count - this.numSuitesPassed;
+                },
+                enumerable: true
+            },
+        });
+    }
+
+    [TEST_CASE_DEFINE](suite, name, fn) {
+        _ensureArgsFormat(name, fn);
+        suite[TEST_SUITE_DEFINE](name, fn);
+        return this;
+    }
+
+    [TEST_SUITE_DEFINE](name, fn, idx = 0) {
+        _ensureArgsFormat(name, fn);
+        this[DATA].suitesMap.set(name, new TestSuite({name, value: fn, idx}));
+    }
+
+    [TEST_SUITE_RUN](suite) {
+        const {name, value: fn} = suite;
+
         _ensureArgsFormat(name, fn);
 
         console.group(name);
